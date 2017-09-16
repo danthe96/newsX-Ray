761077991e4e0503408340e18579d0d8fdf7d973
Daniel Theveßen <danthe96@gmx.de>
2017-09-16 19:19:01 +0200

    Got backend working

--- hello.py
+++ hello.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+import subprocess
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -72,6 +73,26 @@ def calculate_similarity(vec1, vec2):
 def check_similarity():
     reuters_text, original_r_s = clean_text(request.json['reuters'])
     news_text, original_n_s = clean_text(request.json['news'])
+
+    reuters_id = request.json['reuters_id']
+    newssource = request.json['source']
+
+    if os.path.isfile(reuters_id + '_' + newssource + '_vec1.npy'):
+        reuters_data = np.load(reuters_id + '_' + newssource + '_vec1.npy')
+        news_data = np.load(reuters_id + '_' + newssource + '_vec2.npy')
+    else:
+        cutoff = len(reuters_text.split('\n'))
+        f = open('text_temp', 'w')
+        f.write(reuters_text)
+        f.write(news_text)
+        f.close()
+        subprocess.Popen("fasttext/fasttext print-sentence-vectors fasttext/models/wiki.en.bin < text_temp > temp_result")
+
+        result = pd.read_csv('temp_result', sep='\s+', quotechar='"', index_col=0, header=None)
+        reuters_data = result.iloc[:(cutoff - 1)]
+        news_data = result.iloc[cutoff:]
+        np.save(reuters_id + '_' + newssource + '_vec1.npy', reuters_data)
+        np.save(reuters_id + '_' + newssource + '_vec2.npy', news_data)
 
     reuters_sentences = reuters_data.index
     news_sentences = news_data.index
