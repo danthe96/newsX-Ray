099d2369c1ae870d22bd4eebf12af5f0f03d732c
Daniel Theveßen <danthe96@gmx.de>
2017-09-16 20:01:46 +0200

    Fixed backend bugs

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
-from cloudant import Cloudant
 from flask import Flask, render_template, request, jsonify
 import atexit
-import cf_deployment_tracker
 import os
 import json
 import nltk
@@ -10,9 +8,6 @@ import pandas as pd
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 import subprocess
-
-# Emit Bluemix deployment event
-cf_deployment_tracker.track()
 
 app = Flask(__name__)
 
@@ -24,27 +19,6 @@ db = None
 
 SIMILARITY_THRESHOLD = 0.88
 
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
@@ -54,11 +28,11 @@ stop = set(stopwords.words('english'))
 def clean_text(text):
     original_mapping = {}
     text = text.replace('<p>', '').replace('</p>', '').replace('<p/>', '')
-    sentences = tokenizer.tokenize(data)
+    sentences = tokenizer.tokenize(text)
     basic_sentences = []
     for sentence in sentences:
         sentence2 = sentence.replace('\\n', ' ').replace('\\"', '\'').replace('\"', '\'')
-        sentence2 = ' '.join([i for i in line.lower().split() if i not in stop])
+        sentence2 = ' '.join([i for i in sentence2.lower().split() if i not in stop])
         basic_sentences.append(sentence2)
         original_mapping[sentence2] = sentence
 
@@ -66,7 +40,7 @@ def clean_text(text):
 
 
 def calculate_similarity(vec1, vec2):
-    return np.dot(vec1, vec2) / np.linalg.norm(vec1) * np.linalg.norm(vec2)
+    return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
 
 
 @app.route('/', methods=['POST'])
@@ -77,22 +51,23 @@ def check_similarity():
     reuters_id = request.json['reuters_id']
     newssource = request.json['source']
 
-    if os.path.isfile(reuters_id + '_' + newssource + '_vec1.npy'):
-        reuters_data = np.load(reuters_id + '_' + newssource + '_vec1.npy')
-        news_data = np.load(reuters_id + '_' + newssource + '_vec2.npy')
+    if os.path.isfile(reuters_id + '_' + newssource + '_vec1.dat'):
+        reuters_data = pd.read_pickle(reuters_id + '_' + newssource + '_vec1.dat')
+        news_data = pd.read_pickle(reuters_id + '_' + newssource + '_vec2.dat')
     else:
         cutoff = len(reuters_text.split('\n'))
         f = open('text_temp', 'w')
         f.write(reuters_text)
         f.write(news_text)
         f.close()
-        subprocess.Popen("fasttext/fasttext print-sentence-vectors fasttext/models/wiki.en.bin < text_temp > temp_result")
+        p = subprocess.Popen('fasttext/fasttext print-sentence-vectors fasttext/models/wiki.en.bin < text_temp > temp_result', shell=True)
+        p.wait()
 
         result = pd.read_csv('temp_result', sep='\s+', quotechar='"', index_col=0, header=None)
         reuters_data = result.iloc[:(cutoff - 1)]
         news_data = result.iloc[cutoff:]
-        np.save(reuters_id + '_' + newssource + '_vec1.npy', reuters_data)
-        np.save(reuters_id + '_' + newssource + '_vec2.npy', news_data)
+        reuters_data.to_pickle(reuters_id + '_' + newssource + '_vec1.dat')
+        news_data.to_pickle(reuters_id + '_' + newssource + '_vec2.dat')
 
     reuters_sentences = reuters_data.index
     news_sentences = news_data.index
@@ -101,8 +76,9 @@ def check_similarity():
     for n_s in news_sentences:
         for r_s in reuters_sentences:
             similarity = calculate_similarity(reuters_data.loc[r_s], news_data.loc[n_s])
+
             if similarity > SIMILARITY_THRESHOLD:
-                info_in_both += {'news_sentence': original_n_s[n_s], 'original_sentence': original_r_s[r_s], 'score': similarity}
+                info_in_both.append({'news_sentence': original_n_s[n_s], 'reuters_sentence': original_r_s[r_s], 'score': similarity})
 
     info_in_news = [original_n_s[n_s] for n_s in news_sentences if not any(x for x in info_in_both if x['news_sentence'] == n_s)]
     info_in_reuters = [original_r_s[r_s] for r_s in reuters_sentences if not any(x for x in info_in_both if x['reuters_sentence'] == r_s)]
