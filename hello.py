eee65a1cf04fbbecbedebc758713e0f92f93b726
Daniel Theveßen <danthe96@gmx.de>
2017-09-16 18:39:50 +0200

    Sentence mapping

--- hello.py
+++ hello.py
@@ -7,6 +7,8 @@ import json
 import nltk
 import numpy as np
 import pandas as pd
+from nltk import word_tokenize
+from nltk.corpus import stopwords
 
 # Emit Bluemix deployment event
 cf_deployment_tracker.track()
@@ -19,7 +21,7 @@ db_name = 'mydb'
 client = None
 db = None
 
-SIMILARITY_THRESHOLD = 0.8
+SIMILARITY_THRESHOLD = 0.88
 
 if 'VCAP_SERVICES' in os.environ:
     vcap = json.loads(os.getenv('VCAP_SERVICES'))
@@ -45,13 +47,21 @@ elif os.path.isfile('vcap-local.json'):
 # On Bluemix, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
 port = int(os.getenv('PORT', 8000))
+stop = set(stopwords.words('english'))
 
 
 def clean_text(text):
-    text = text.replace('<p>', '').replace('</p>', '').replace('<p/>', '') \
-        .replace('\\n', ' ').replace('\\"', '\'').replace('\"', '\'')
+    original_mapping = {}
+    text = text.replace('<p>', '').replace('</p>', '').replace('<p/>', '')
     sentences = tokenizer.tokenize(data)
-    return ''.join(['\"' + s + '\"\n' for s in sentences])
+    basic_sentences = []
+    for sentence in sentences:
+        sentence2 = sentence.replace('\\n', ' ').replace('\\"', '\'').replace('\"', '\'')
+        sentence2 = ' '.join([i for i in line.lower().split() if i not in stop])
+        basic_sentences.append(sentence2)
+        original_mapping[sentence2] = sentence
+
+    return (''.join(['\"' + s + '\"\n' for s in basic_sentences]), original_mapping)
 
 
 def calculate_similarity(vec1, vec2):
@@ -60,8 +70,8 @@ def calculate_similarity(vec1, vec2):
 
 @app.route('/', methods=['POST'])
 def check_similarity():
-    reuters_text = clean_text(request.json['reuters'])
-    news_text = clean_text(request.json['news'])
+    reuters_text, original_r_s = clean_text(request.json['reuters'])
+    news_text, original_n_s = clean_text(request.json['news'])
 
     reuters_sentences = reuters_data.index
     news_sentences = news_data.index
@@ -71,10 +81,10 @@ def check_similarity():
         for r_s in reuters_sentences:
             similarity = calculate_similarity(reuters_data.loc[r_s], news_data.loc[n_s])
             if similarity > SIMILARITY_THRESHOLD:
-                info_in_both += {'news_sentence': n_s, 'original_sentence': r_s, 'score': similarity}
+                info_in_both += {'news_sentence': original_n_s[n_s], 'original_sentence': original_r_s[r_s], 'score': similarity}
 
-    info_in_news = [n_s for n_s in news_sentences if not any(x for x in info_in_both if x['news_sentence'] == n_s)]
-    info_in_reuters = [r_s for r_s in reuters_sentences if not any(x for x in info_in_both if x['reuters_sentence'] == r_s)]
+    info_in_news = [original_n_s[n_s] for n_s in news_sentences if not any(x for x in info_in_both if x['news_sentence'] == n_s)]
+    info_in_reuters = [original_r_s[r_s] for r_s in reuters_sentences if not any(x for x in info_in_both if x['reuters_sentence'] == r_s)]
 
     return jsonify({
         'matched_sentences': info_in_both,
